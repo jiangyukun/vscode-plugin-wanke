e936af6731ef071366337b535c9b1cf25900fd8c
jiangyukun <187_6810_5877@sina.com>
2019-11-02 11:57:14 +0800

    模块重新排序

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
-import { TextEditor, TextEditorEdit, Position, TextLine } from 'vscode';
-import { type } from 'os';
+import { TextEditor, TextEditorEdit, Range,Position, TextLine } from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "vscode-plugin-wanke" is now active!');
@@ -18,71 +17,85 @@ export function activate(context: vscode.ExtensionContext) {
 		for (let line = 0; line < textEditor.document.lineCount; line++) {
 			let textLine: TextLine = textEditor.document.lineAt(line)
 			if (textLine.text.indexOf('import ') != -1) {
-				edit.delete(textLine.range)
+				edit.delete(new Range(line, 0 ,line+1, 0))
+			} else if (textLine.text.trim() == '') {
+				edit.delete(new Range(line, 0 ,line+1, 0))
+			} else {
+				break
 			}
 		}
+
 
 		let thirdModules = []
 		let localModules = []
 		let helpAndContants = []
-		let types = []
+		let typeModules = []
+		let otherModules = []
 
 		for (let importStr of importList) {
 			let match = importStr.match(/'(.*)'/)
 			if (match && match[1]) {
 				let from = match[1]
-				console.log('from ------ ' + from)
 
 				if (from.indexOf('./') == -1 && from.indexOf('../') == -1) {
 					thirdModules.push(importStr)
 					continue
 				}
 
-				if (from.indexOf('type') != -1) {
-					types.push(importStr)
+				if (from.indexOf('api-ts') != -1 || from.indexOf('interfaces') != -1) {
+					typeModules.push(importStr)
 					continue
 				}
 				if (from.indexOf('./') != -1 || from.indexOf('../') != -1) {
-					let match1 = from.match(/\/(\w*)$/)
-					if (match1 && match1[1]) {
-						let moduleName = match1[1]
-						console.log('moduleName ---- ' + moduleName)
-						if (moduleName.charAt(0) > 'a' && moduleName.charAt(0) < 'z') {
-							helpAndContants.push(importStr)
-						}
-						if (moduleName.charAt(0) > 'A' && moduleName.charAt(0) < 'Z') {
-							localModules.push(importStr)
-						}
+					let index = from.lastIndexOf('/')
+					let moduleName = from.substring(index + 1)
+
+					if (moduleName.charAt(0) >= 'a' && moduleName.charAt(0) <= 'z') {
+						helpAndContants.push(importStr)
+					}
+					if (moduleName.charAt(0) >= 'A' && moduleName.charAt(0) <= 'Z') {
+						localModules.push(importStr)
 					}
 					continue
 				}
+				otherModules.push(importStr)
 			}
 		}
 
 		let line = 0
 
-		for (let txt of thirdModules) {
-			edit.insert(new Position(line++, 0), `${txt}`)
-		}
-		if (localModules.length > 0) {
-			edit.insert(new Position(line++, 0), ``)
-			for (let txt of localModules) {
-				edit.insert(new Position(line++, 0), `${txt}`)
+		if(thirdModules.length > 0) {
+			for (let txt of thirdModules) {
+				edit.insert(new Position(line++, 0), `${txt}\n`)
 			}
+			edit.insert(new Position(line++, 0), `\n`)
+		}
+		
+		if (localModules.length > 0) {
+			for (let txt of localModules) {
+				edit.insert(new Position(line++, 0), `${txt}\n`)
+			}
+			edit.insert(new Position(line++, 0), `\n`)
 		}
 
 		if (helpAndContants.length > 0) {
-			edit.insert(new Position(line++, 0), ``)
 			for (let txt of helpAndContants) {
-				edit.insert(new Position(line++, 0), `${txt}`)
+				edit.insert(new Position(line++, 0), `${txt}\n`)
 			}
+			edit.insert(new Position(line++, 0), `\n`)
 		}
 
-		if (types.length > 0) {
-			edit.insert(new Position(line++, 0), ``)
-			for (let txt of types) {
-				edit.insert(new Position(line++, 0), `${txt}`)
+		if (typeModules.length > 0) {
+			for (let txt of typeModules) {
+				edit.insert(new Position(line++, 0), `${txt}\n`)
 			}
+			edit.insert(new Position(line++, 0), `\n`)
+		}
+		if (otherModules.length > 0) {
+			for (let txt of otherModules) {
+				edit.insert(new Position(line++, 0), `${txt}\n`)
+			}
+			edit.insert(new Position(line++, 0), `\n`)
 		}
 
 	});
